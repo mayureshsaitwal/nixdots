0a98333d6f834c9a6cb801b65526ad059b92f7fd
mayureshsaitwal <mayureshsaitwal25>
2026-09-11 14:00:43 +0530

    :gear: feat (store) add store to git

--- modules/git/git.nix
+++ modules/git/git.nix
@@ -1,11 +1,17 @@
 {
   programs.git = {
+    enable = true;
+    userName = "mayureshsaitwal";
+    userEmail = "mayureshsaitwal25@gmail.com";
     aliases = {
       ga = "add";
       gci = "commit";
       gco = "checkout";
       gs = "status";
       gp = "push";
+    };
+    extraConfig = {
+      credential.helper = "store";
     };
   };
 }
