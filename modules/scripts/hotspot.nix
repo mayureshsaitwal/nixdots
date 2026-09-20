4fc73d25e4567dc5d9a97c7ba2b671fe79956335
mayureshsaitwal <mayureshsaitwal25>
2026-09-20 21:57:14 +0530

    :gear: feat(hotspot) hotspot run on startup

--- modules/scripts/hotspot.nix
+++ modules/scripts/hotspot.nix
@@ -15,7 +15,7 @@
     };
 
     script = ''
-      lnxrouter --ap wlo1 "Bathroom Camera #4" -p "Qwerty?123" --daemon
+      lnxrouter --ap wlo1 "Munni Badnaam Hui, Router Ke Liye" -p "Qwerty?123" --daemon
     '';
   };
 }
